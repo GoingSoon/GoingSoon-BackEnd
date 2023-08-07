c7689514273e9d94b25d66f2bbf7b35ceec0625a
EunKyung Lee <2dmsrud2002@naver.com>
2023-08-07 10:34:47 +0900

    member entity

--- src/main/java/com/gujilgujil/domain/member/Member.java
+++ src/main/java/com/gujilgujil/domain/member/Member.java
@@ -1,4 +1,26 @@
 package com.gujilgujil.domain.member;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String memberId;
+    private String password;
+    private String image;
+    private String nickname;
 }
