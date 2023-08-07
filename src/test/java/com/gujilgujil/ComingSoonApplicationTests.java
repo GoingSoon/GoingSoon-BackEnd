4daca42f3cbad35d92923df289473c79e7be1e28
EunKyung Lee <2dmsrud2002@naver.com>
2023-08-07 10:03:31 +0900

    project setting

---
+++ src/test/java/com/gujilgujil/ComingSoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gujilgujil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComingSoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
