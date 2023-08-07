abf0429016be1b6b2a9e38de1b4b611ef81097c5
EunKyung Lee <2dmsrud2002@naver.com>
2023-08-07 10:35:55 +0900

    team entity

---
+++ src/main/java/com/gujilgujil/domain/team/Team.java
@@ -0,0 +1,24 @@
+package com.gujilgujil.domain.team;
+
+import com.gujilgujil.domain.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
