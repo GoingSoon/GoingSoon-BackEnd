06b67b9b482ef1c4c790ba7c542bf6e5694a113c
sed-int <nouveau0309@gmail.com>
2023-08-08 01:15:50 +0900

    [feat] : Add FriendAlertRepoitory, FriendAlertService

---
+++ src/main/java/com/gujilgujil/domain/friendAlert/FriendAlertRepository.java
@@ -0,0 +1,6 @@
+package com.gujilgujil.domain.friendAlert;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendAlertRepository extends JpaRepository<FriendAlert, Long> {
+}
---
+++ src/main/java/com/gujilgujil/domain/friendAlert/FriendAlertService.java
@@ -0,0 +1,9 @@
+package com.gujilgujil.domain.friendAlert;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class FriendAlertService {
+}
